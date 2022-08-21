bdcbb7f1e7ffa4f0f955b0981b0be008a71e35da
Ryan Jacobs <ryry22892@gmail.com>
2022-08-21 12:15:18 -0400

    update Rmd file

---
+++ common_variables.R
@@ -0,0 +1,10 @@
+# Common non-csv data between CV and resume
+
+# Header with description of what is trying to be accomplished with this CV/Resume
+intro_text <- "**Data science professional driving value for companies daily.** My expertise covers multiple areas: defining KPIs and extracting insights with imperfect data; analyzing A/B experiments to make product improves and drive sustainable growth.
+
+I've utilized these skills across multiple cross-functional teams across a variety of industries - including ed tech, ad tech, traditional media and digital publishing."
+
+
+
+
